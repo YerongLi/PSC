9716951caeba8755b34f9c8742a25db16947b696
Yerong <yerong.li@outlook.com>
2018-11-15 22:45:00 -0600

    modify script

--- step1.m
+++ step1.m
@@ -1,8 +1,8 @@
 % excute the job in dqshtc
 
-DIR= '/home/yl148/PSC';
+DIR= '/home/yl148/PSC/';
 %datadir=fullfile(DIR,'data')
-datadir=fullfile('../','data');
+datadir=fullfile(DIR,'data');
 %datadir=fullfile(DIR,'data2');
 subID=dir(datadir);
 subID={subID.name};
@@ -12,7 +12,7 @@ L=length(subID);
 scriptdir =fullfile(DIR,'Scilpy/');
 scriptdir1 =fullfile(scriptdir,'PSC_Pipeline/UK_Biobank/');
 
-codedir = fullfile(DIR,'code12');
+codedir = fullfile(DIR,'code');
 %codedir = fullfile(DIR,'code1');
 
 if exist(codedir)
@@ -22,12 +22,14 @@ mkdir(codedir)
 
 fid0 = fopen(fullfile(codedir,'All_step1.sh'),'w');
 fprintf(fid0,'#!/bin/tcsh\n');
+
+disp(L)
 for i=1:L
     sub_id = subID{i};
-	temp=fullfile(datadir,sub_id,'AP.nii.gz')
-	temp1=fullfile(datadir,sub_id,'data.nii.gz')
-	temp2=fullfile(datadir,sub_id,'nodif.nii.gz')
-	if (~exist(temp)) & (~exist(temp1)) & (~exist(temp2))
+	temp=fullfile(datadir,sub_id,'AP.nii.gz');     % one of them should work
+	temp1=fullfile(datadir,sub_id,'data.nii.gz');  % one of them should work
+	temp2=fullfile(datadir,sub_id,'nodif.nii.gz'); % one of them should work
+        if (~exist(temp)) & (~exist(temp1)) & (~exist(temp2))
 	continue;
 	end
 	temp=fullfile(datadir,sub_id);
@@ -64,10 +66,10 @@ for i=1:L
     fprintf(fid, '#SBATCH --mail-type=ALL\n');
 
     %%fprintf(fid,'#PBS -N Zhengwu_%s\n',subID{i});
-    fprintf(fid,'#SBATCH --job-name=PSC%s\n',subID{i});
+    fprintf(fid,'#SBATCH --job-name=%s\n',subID{i});
     %%fprintf(fid,'#PBS -o Zhengwu_%s.out\n',subID{i});
     fprintf(fid,'#SBATCH -o PSC%s.out\n',subID{i});
-
+    fprintf(fid,'#SBATCH -e err%s\n',subID{i});
     %%fprintf(fid,'#PBS -j oe\n');
     fprintf(fid, '# sub job for subject %s \n', sub_id);
     fprintf(fid,'module load GCC/6.4.0  OpenMPI/2.1.3 MRtrix/0.3.15 GSL/2.4  ANTs/2.1.0 FreeSurfer/6.0.0 FSL\n');
@@ -76,15 +78,15 @@ for i=1:L
     %%fprintf(fid,'module load mrtrix\n');
     %%fprintf(fid,'module load FSL\n');
     %%fprintf(fid,'module load ANTs/2.1.0\n');
-    fprintf(fid,'setenv PATH /workspace/tli3/software/Anaconda2_for_scilpy2017/Anaconda2/bin:$PATH \n');
-    fprintf(fid,'setenv PATH %s/scripts/:${PATH} \n',scriptdir);
-    fprintf(fid,'setenv PYTHONPATH %s/ \n',scriptdir);
+    %%fprintf(fid,'setenv PATH /workspace/tli3/software/Anaconda2_for_scilpy2017/Anaconda2/bin:$PATH \n');
+    fprintf(fid,'export PATH=/home/yl148/PSC/Scilpy/scripts/:$PATH \n',scriptdir);
+    fprintf(fid,'export PYTHONPATH=%s \n',scriptdir);
     fprintf(fid,'cd %s/%s \n',datadir,sub_id);
     %remove output1 and prepare for re-write
     fprintf(fid,'chmod 775 %s/PREPROCESS_Step1_Registration.sh \n',scriptdir1);
 	
 	fprintf(fid,'mv dwi.nii.gz data.nii.gz \n');
-	fprintf(fid,'mv nodif.nii.gz data.nii.gz \n');
+	fprintf(fid,'mv nodif.nii.gz t1.nii.gz \n');
 	fprintf(fid,'mv bvecs0 bvecs \n');
 	fprintf(fid,'mv bvals0 bvals \n');
 	
